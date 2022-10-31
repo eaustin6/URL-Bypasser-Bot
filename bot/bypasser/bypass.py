33f59cd54258cba6463075c419682fdb3875f48b
itzkakarotto7@gmail.com <itzkakarotto7@gmail.com>
2022-10-31 12:26:52 +0530

    g

--- bot/bypasser/bypass.py
+++ bot/bypasser/bypass.py
@@ -7,22 +7,26 @@ import PyBypass as bypasser
 def bypass(_, msg):
     if msg.reply_to_message:
         url = msg.reply_to_message.text
+        if len(msg.text.split())>1:
+            nam = msg.text.split()[1]
+        else:
+            nam = None
     else:
         try:
             url = msg.text.split()[1]
             try:
-                name = msg.text.split()[2]
+                nam = msg.text.split()[2]
             except:
-                None
+                nam=None
         except:
             return msg.reply_text("Please Reply to a Url")
 
-    x = msg.reply(f"Bypassing {url}...")
+    x = msg.reply(f"Bypassing __`{url}`__...")
 
     try:
         bypassed = bypasser.bypass(url)
         if name:
-            bypassed = bypasser.bypass(url, name=name)
+            bypassed = bypasser.bypass(url, name=nam)
     except Exception as e:
         return msg.reply(e)
 
