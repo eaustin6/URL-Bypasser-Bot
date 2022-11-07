4bd19c96030d68ee697c4e67aac4dcb731346905
programmingcloud7@gmail.com <programmingcloud7@gmail.com>
2022-11-08 00:23:21 +0530

    f

--- bot/bypasser/imgtopdf.py
+++ bot/bypasser/imgtopdf.py
@@ -5,6 +5,12 @@ from bot import app
 from pyrogram import filters
 import os
 
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'), ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'), ('Accept-Encoding','gzip, deflate, br'),\
+    ('Accept-Language','en-US,en;q=0.5' ), ("Connection", "keep-alive"), ("Upgrade-Insecure-Requests",'1')]
+urllib.request.install_opener(opener)
+
+
 
 def save(imgurl, filename):
     urllib.request.urlretrieve(imgurl, filename+".jpg")
