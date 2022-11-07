b07197867cc11edc9e29f325efc91740cb413d34
programmingcloud7@gmail.com <programmingcloud7@gmail.com>
2022-11-07 22:38:03 +0530

    f

--- bot/bypasser/imgtopdf.py
+++ bot/bypasser/imgtopdf.py
@@ -3,6 +3,7 @@ import requests
 import img2pdf
 from bot import app
 from pyrogram import filters
+import os
 
 
 def save(imgurl, filename):
@@ -13,18 +14,19 @@ def save(imgurl, filename):
 @app.on_message(filters.command('imgtopdf'))
 def convertPDF(_, message):
     msg = message.text.split("&")
-    data= msg[1].replace("['", "").replace("']", "").split("', '")
+    data= msg[1].replace("['", "").replace("']", "").replace(";", "").split("', '")
     name = msg[2]
-    
+    print(os.listdir())
     print(data, name)
     try:
         for _ in data:
             flnm=f"{name}/{data.index(_)}"
             print(flnm)
             save(_, flnm)
+        print(os.listdir())
     except:
         message.reply("Please enter valid data")
-        
+    print(os.listdir())
     with open(f"{name}.pdf","wb") as f:
         f.write(img2pdf.convert(glob.glob(f"{name}/*.jpg")))
     message.reply_document(f"{name}.pdf")
