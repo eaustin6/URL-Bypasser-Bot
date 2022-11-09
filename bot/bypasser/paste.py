7baef0d2b883194a07ff026aaec45ded64ad56b4
Anshu <81678105+ItzKakarotto@users.noreply.github.com>
2022-11-09 19:30:10 +0530

    Create paste.py

---
+++ bot/bypasser/paste.py
@@ -0,0 +1,14 @@
+import requests
+
+def paste(text):
+    url = "https://spaceb.in/api/v1/documents/"
+    res = requests.post(url, data={"content": text, "extension": "txt"})
+    return f"https://spaceb.in/{res.json()['payload']['id']}"
+
+@app.on_message(filters.command('paste'))
+def pastewo(_, message):
+    if message.reply_to_message:
+        text = message.reply_to_message.text
+    else:
+        text = message.text.split()[1]
+    return paste(text)
